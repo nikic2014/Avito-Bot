4793355b5f8824c74975a19716e66c089f4eff82
Nikita <niki.c2000@yandex.ru>
2023-03-18 03:23:01 +0400

    запаршена вся информация по машинам, есть переход на следующую страницу с машинами. Пойман бан по ip

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram_calendar import simple_cal_callback, SimpleCalendar, \
     dialog_cal_callback, DialogCalendar
@@ -11,7 +9,8 @@ import psycopg2
 import datetime
 import asyncio
 import threading
-
+import time
+from multiprocessing import Process
 
 import const
 import GUI
@@ -34,7 +33,6 @@ conaction = psycopg2.connect(
 print("Соединение открыто")
 
 
-
 def BOT():
     try:
         @dp.message_handler(commands=['start'])
@@ -46,25 +44,21 @@ def BOT():
                                  reply_markup=GUI.main_kb,
                                  parse_mode="HTML")
 
-
         @dp.message_handler(text='Квартиры')
         async def apartment(message: types.Message):
             await message.answer("Выберите количество комнат",
                                  reply_markup=GUI.apartment_kb)
 
-
         @dp.message_handler(text='Машины')
         async def car(message: types.Message):
-            await bot.send_photo(message.chat.id, "https://10.img.avito.st/image/1/1.mzKEXLa3N9uk_ltVm3qWWjP_N9E6aTS3Mv8.hQrExpzBoBTeGM3wQ-qCwTO8oObnvB3_7TERO_0_r2w")
+            await bot.send_photo(message.chat.id,
+                                 "https://10.img.avito.st/image/1/1.mzKEXLa3N9uk_ltVm3qWWjP_N9E6aTS3Mv8.hQrExpzBoBTeGM3wQ-qCwTO8oObnvB3_7TERO_0_r2w")
             await message.answer("Выберите ценовой сегмент",
                                  reply_markup=GUI.car_kb)
-
-
 
         @dp.message_handler(text='Назад')
         async def back_in_main(message: types.Message):
             await message.answer("Вы в главном меню", reply_markup=GUI.main_kb)
-
 
         @dp.message_handler(text='Связь с разработчиком')
         async def contact_developer(message: types.Message):
@@ -78,15 +72,18 @@ def BOT():
 
     executor.start_polling(dp, skip_updates='true')
 
-
-from multiprocessing import Process
-
+def call_parse():
+    while True:
+        parser_selenium.test_parse(
+            "https://www.avito.ru/saratov/avtomobili?cd=1&p=&radius=50&searchRadius=50")
+        parser_selenium.test_parse("https://www.avito.ru/saratov/avtomobili/do-300000-rubley-ASgCAgECAUXGmgwWeyJmcm9tIjowLCJ0byI6MzAwMDAwfQ?cd=1&p=&radius=50&searchRadius=50")
+        asyncio.sleep(79200) ### Парсинг каждые 22 часа
 
 if __name__ == '__main__':
-  p1 = Process(target=BOT)
-  p1.start()
-  p2 = Process(target=parser_selenium.test_parse("https://www.avito.ru/saratov/avtomobili?cd=1&radius=50&searchRadius=5"))
-  p2.start()
-  p1.join()
-  p2.join()
-
+    # p1 = Process(target=BOT)
+    # p1.start()
+    # p2 = Process(target=call_parse())
+    # p2.start()
+    # p1.join()
+    # p2.join()
+    call_parse()
--- parsers/parser_selenium.py
+++ parsers/parser_selenium.py
@@ -2,8 +2,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from parsers.parser_beautiful_soup import parse_current_car
+# from parsers.parser_beautiful_soup import parse_current_car
 
+"""
 def parse_car():
     browser = webdriver.Chrome()
 
@@ -42,16 +43,37 @@ def parse_car():
 
     # links = browser.find_elements(By.CLASS_NAME, "title-info-title-text")
     # print(links[0].get_attribute("innerHTML"))
+"""
 
-# parse_car()
 
-def parse_image(URL):
+def parse_info(URL):
     browser = webdriver.Chrome()
     browser.get(URL)
 
     img_array = []
 
-    # if (browser.find_element(By.CLASS_NAME, "closed-warning-linkText-bXEpX"))
+    title = browser.find_element(By.CLASS_NAME,
+                                 "title-info-title-text").get_attribute(
+        "innerHTML")
+    print(title)
+
+    description = None
+    try:
+        description = browser.find_element(By.CLASS_NAME,
+                                           "style-item-description"
+                                           "-text-mc3G6").find_element(
+            By.TAG_NAME, "p").get_attribute("innerHTML")
+    except:
+        description = browser.find_element(By.CLASS_NAME,
+                                           "style-item-description"
+                                           "-html-qCwUL").find_element(
+            By.TAG_NAME, "p").get_attribute("innerHTML")
+    print(description)
+
+    price = browser.find_element(By.CLASS_NAME,
+                                 "style-price-value-main-TIg6u").find_element(
+        By.TAG_NAME, "span").get_attribute("content")
+    print(price)
 
     while 1:
         try:
@@ -67,32 +89,40 @@ def parse_image(URL):
             break
 
         try:
-            btn = browser.find_elements(By.CLASS_NAME, "image-frame-controlButton-_vPNK")
+            btn = browser.find_elements(By.CLASS_NAME,
+                                        "image-frame-controlButton-_vPNK")
             btn[1].click()
             time.sleep(0.5)
         except:
             return []
-    return img_array
+    return (img_array, title, price, description)
 
 
 def test_parse(URL):
-    browser = webdriver.Chrome()
+    page = 30
+    while True:
+        try:
+            URL = str(URL).replace("p=", f"p={page}")
 
-    browser.get(URL)
-    links = []
-    cars = browser.find_elements(By.CLASS_NAME, "iva-item-titleStep-pdebR")
-    for i in cars:
-        link = i.find_element(By.TAG_NAME, "a").get_attribute("href")
-        links.append(link)
+            browser = webdriver.Chrome()
+            browser.get(URL)
 
-    browser.quit()
+            links = []
+            cars = browser.find_elements(By.CLASS_NAME, "iva-item-titleStep-pdebR")
+            for i in cars:
+                link = i.find_element(By.TAG_NAME, "a").get_attribute("href")
+                links.append(link)
 
-    for i in links:
-        images = parse_image(i)
-        if images == []:
-            print("Bug fixed", i)
-            continue
-        time.sleep(3)
+            browser.quit()
 
+            for i in links:
+                images = parse_info(i)
+                if images == []:
+                    print("Bug fixed", i)
+                    continue
+                time.sleep(3)
+                print(images)
 
-#test_parse("https://www.avito.ru/saratov/avtomobili?cd=1&radius=50&searchRadius=50")
+            page += 1
+        except:
+            break
